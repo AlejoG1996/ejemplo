eac8b0b9538c36ed6caa0a3ae5ad9232d0c6cd8a
AlejoG1996 <galeanomadrigalalejnadro@gmail.com>
2021-08-18 21:55:14 -0500

    add entity

---
+++ ejemplo.Common/Models/clase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ejemplo.Common.Models
+{
+    public class clase
+    {
+        public DateTime CreateTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsComplated { get; set; }
+    }
+}
---
+++ ejemplo.Common/Responses/response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejemplo.Common.Responses
+{
+    public class response
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object  resultado  { get; set; }
+    }
+}
